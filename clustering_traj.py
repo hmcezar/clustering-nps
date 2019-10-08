3d65470e1f59814500ba7ed02c399c017c0a40cf
Henrique Musseli Cezar <henrique.musseli@gmail.com>
2019-10-08 12:29:59 -0300

    Fixed infinite iterator and removed unecessary flags.

--- clustering_traj.py
+++ clustering_traj.py
@@ -25,26 +25,6 @@ import matplotlib.pyplot as plt
 import multiprocessing
 import itertools
 
-def get_mol_coords(mol):
-  q_all = []
-  for atom in mol:
-    q_all.append(atom.coords)
-
-  return np.asarray(q_all)
-
-
-def get_mol_info(mol):
-  # table to convert atomic number to symbols
-  etab = openbabel.OBElementTable()
-
-  q_atoms = []
-  q_all = []
-  for atom in mol:
-    q_atoms.append(etab.GetSymbol(atom.atomicnum))
-    q_all.append(atom.coords)
-
-  return np.asarray(q_atoms), np.asarray(q_all)
-
 
 def get_atom_dist(r1, r2):
   return np.linalg.norm(np.asarray(r1)-np.asarray(r2))
@@ -62,24 +42,24 @@ def get_adists_mol(mol):
 
   return sorted(adist)
 
+
 def wrapmol(mol):
   dictmol = {}
   for idx, atom in enumerate(mol.atoms):
     dictmol[idx] = [atom.atomicnum, atom.coords]
   return dictmol
 
-def build_distance_matrix(trajfile, noh, reorder, natoms, nprocs):
-  # create iterator containing information to compute a line of the distance matrix
-  # inputiterator = zip(itertools.count(), itertools.repeat(adists_dict), itertools.repeat(trajfile), itertools.repeat(noh), itertools.repeat(reorder), itertools.repeat(natoms))
 
+def build_distance_matrix(trajfile, nprocs):
   # create the pool with nprocs processes to compute the distance matrix in parallel
   p = multiprocessing.Pool(processes = nprocs)
 
-  # calculate the atom distances
+  # calculate the atom distances and create iterator
   adists = p.map(get_adists_mol, map(lambda x: wrapmol(x), pybel.readfile(os.path.splitext(trajfile)[1][1:], trajfile)))
 
+  inputiterator = zip(itertools.count(), itertools.repeat(adists, len(adists)))
+
   # build the distance matrix in parallel
-  inputiterator = zip(itertools.count(), itertools.repeat(adists))  
   ldistmat = p.starmap(compute_distmat_line, inputiterator)
 
   return np.asarray([x for n in ldistmat if len(n) > 0 for x in n])
@@ -89,18 +69,14 @@ def compute_distmat_line(idx1, adists):
   # initialize distance matrix
   distmat = []
 
-  for idx2 in range(len(adists)):
-    # skip if it's not an element from the superior diagonal matrix
-    if idx1 >= idx2:
-      continue 
-
+  for idx2 in range(idx1+1, len(adists)):
     # get the distance and store
-    distmat.append(np.sum(np.asarray(adist[idx1])-np.asarray(adist[idx2])))
+    distmat.append(np.sum(np.square(np.asarray(adists[idx1])-np.asarray(adists[idx2]))))
 
   return distmat
 
 
-def save_clusters_config(trajfile, clusters, distmat, noh, reorder, nsatoms, outbasename, outfmt):
+def save_clusters_config(trajfile, clusters, distmat, outbasename, outfmt):
 
   # complete distance matrix
   sqdistmat = squareform(distmat)
@@ -111,7 +87,7 @@ def save_clusters_config(trajfile, clusters, distmat, noh, reorder, nsatoms, out
     outfile = pybel.Outputfile(outfmt,outbasename+"_"+str(cnum)+"."+outfmt)
 
     # creates mask with True only for the members of cluster number cnum
-    mask = np.array([1 if i==cnum else 0 for i in clusters],dtype=bool)
+    mask = np.array([1 if i==cnum else 0 for i in clusters], dtype=bool)
 
     # gets the member with smallest sum of distances from the submatrix
     idx = np.argmin(sum(sqdistmat[:,mask][mask,:]))
@@ -120,129 +96,22 @@ def save_clusters_config(trajfile, clusters, distmat, noh, reorder, nsatoms, out
     sublist=[num for (num, cluster) in enumerate(clusters) if cluster==cnum]
     medoid = sublist[idx]
 
-    # get the medoid coordinates
+    # print the medoid coordinates
     for idx, mol in enumerate(pybel.readfile(os.path.splitext(trajfile)[1][1:], trajfile)):
 
       if idx != medoid:
         continue
 
-      # medoid coordinates
-      tnatoms = len(mol.atoms)
-      q_atoms, q_all = get_mol_info(mol)
-
-      if nsatoms:
-        # get the number of non hydrogen atoms in the solute to subtract if needed
-        if noh:
-          natoms = len(np.where(q_atoms[:nsatoms] != 'H')[0])
-        else:
-          natoms = nsatoms
-
-        if noh:
-          not_hydrogens = np.where(q_atoms != 'H')
-          Q = np.copy(q_all[not_hydrogens])
-          Qa = np.copy(q_atoms[not_hydrogens])
-        else:
-          Q = np.copy(q_all)
-          Qa = np.copy(q_atoms)
-
-        qcenter = rmsd.centroid(Q[:natoms])
-      elif noh:
-        not_hydrogens = np.where(q_atoms != 'H')
-        Q = np.copy(q_all[not_hydrogens])
-        qcenter = rmsd.centroid(Q)
-        Qa = np.copy(q_atoms[not_hydrogens])
-      else:
-        Q = np.copy(q_all)
-        qcenter = rmsd.centroid(Q)
-        Qa = np.copy(q_atoms)
-
-      # center the coordinates at the origin
-      Q -= qcenter
-
-      # write medoid configuration to file (molstring is a xyz string used to generate de pybel mol)
-      molstring = str(tnatoms)+"\n"+mol.title.rstrip()+"\n"
-      for i, coords in enumerate(q_all - qcenter):
-        molstring += q_atoms[i]+"\t"+str(coords[0])+"\t"+str(coords[1])+"\t"+str(coords[2])+"\n"
-      rmol = pybel.readstring("xyz", molstring)
-      outfile.write(rmol)
-
+      outfile.write(mol)
       break
 
-    # rotate all the cluster members into the medoid and print them to the .xyz file
+    # print the coordinates of the other NPs
     for idx, mol in enumerate(pybel.readfile(os.path.splitext(trajfile)[1][1:], trajfile)):
 
       if not mask[idx] or idx == medoid:
         continue
 
-      # config coordinates
-      p_atoms, p_all = get_mol_info(mol)
-
-      if nsatoms:
-        if noh:
-          not_hydrogens = np.where(p_atoms != 'H')
-          P = np.copy(p_all[not_hydrogens])
-          Pa = np.copy(p_atoms[not_hydrogens])
-        else:
-          P = np.copy(p_all)
-          Pa = np.copy(p_atoms)     
-
-        pcenter = rmsd.centroid(P[:natoms])
-      elif noh:
-        not_hydrogens = np.where(p_atoms != 'H')
-        P = np.copy(p_all[not_hydrogens])
-        pcenter = rmsd.centroid(P)
-        Pa = np.copy(p_atoms[not_hydrogens])
-      else:
-        P = np.copy(p_all)
-        pcenter = rmsd.centroid(P)
-        Pa = np.copy(p_atoms)
-
-      # center the coordinates at the origin
-      P -= pcenter
-      p_all -= pcenter
-
-      # generate rotation to superpose the solute configuration
-      if nsatoms:
-        # reorder solute atoms
-        prr = reorder(Qa[:natoms], Pa[:natoms], Q[:natoms], P[:natoms])
-        prr = np.concatenate((prr,np.arange(len(P)-natoms)+natoms))
-        P = P[prr]
-        Pa = Pa[prr]
-
-        # generate a rotation considering only the solute atoms
-        U = rmsd.kabsch(P[:natoms], Q[:natoms])
-
-        # rotate the whole system with this rotation
-        P = np.dot(P, U)
-        p_all = np.dot(p_all, U)
-
-        # consider only the solvent atoms in the reorder
-        prr = reorder(Qa[natoms:], Pa[natoms:], Q[natoms:], P[natoms:])
-        prr += natoms
-        prr = np.concatenate((np.arange(natoms),prr))
-        Pr = P[prr]
-        Pra = Pa[prr]
-      # reorder the atoms if necessary
-      elif reorder:
-        prr = reorder(Qa, Pa, Q, P)
-        Pr = P[prr]
-        Pra = Pa[prr]
-      else:
-        Pr = P
-        Pra = Pa
-
-      # generate rotation matrix
-      U = rmsd.kabsch(Pr, Q)
-
-      # rotate whole configuration (considering hydrogens even with noh)
-      p_all = np.dot(p_all, U)
-
-      # write rotated configuration to file (molstring is a xyz string used to generate de pybel mol)
-      molstring = str(tnatoms)+"\n"+mol.title.rstrip()+"\n"
-      for i, coords in enumerate(p_all):
-        molstring += p_atoms[i]+"\t"+str(coords[0])+"\t"+str(coords[1])+"\t"+str(coords[2])+"\n"
-      rmol = pybel.readstring("xyz", molstring)
-      outfile.write(rmol)
+      outfile.write(mol)
 
     # closes the file for the cnum cluster
     outfile.close()
@@ -260,14 +129,10 @@ if __name__ == '__main__':
   parser.add_argument('trajectory_file', help='path to the trajectory containing the conformations to be classified')
   parser.add_argument('min_rmsd', help='value of RMSD used to classify structures as similar')
   parser.add_argument('-np', '--nprocesses', metavar='NPROCS', type=check_positive, default=2, help='defines the number of processes used to compute the distance matrix and multidimensional representation (default = 2)')
-  parser.add_argument('-n', '--no-hydrogen', action='store_true', help='ignore hydrogens when doing the Kabsch superposition and calculating the RMSD')
   parser.add_argument('-p', '--plot', action='store_true', help='enable the multidimensional scaling and dendrogram plot saving the figures in pdf format (filenames use the same basename of the -oc option)')
   parser.add_argument('-m', '--method', metavar='METHOD', default='average', help="method used for clustering (see valid methods at https://docs.scipy.org/doc/scipy-0.19.1/reference/generated/scipy.cluster.hierarchy.linkage.html) (default: average)")
   parser.add_argument('-cc', '--clusters-configurations', metavar='EXTENSION', help='save superposed configurations for each cluster in EXTENSION format (basename based on -oc option)')
   parser.add_argument('-oc', '--outputclusters', default='clusters.dat', metavar='FILE', help='file to store the clusters (default: clusters.dat)')
-  parser.add_argument('-e', '--reorder', action='store_true', help='reorder atoms of molecules to lower the RMSD (default: Hungarian)')
-  parser.add_argument('--reorder-alg', action='store', default="distance", metavar="METHOD", help='select which reorder algorithm to use; hungarian, brute, distance (default). Warning: brute is VERY slow)')
-  parser.add_argument('-ns', '--natoms-solute', metavar="NATOMS", type=int, help='number of solute atoms, to ignore these atoms in the reordering process')
 
   io_group = parser.add_mutually_exclusive_group()
   io_group.add_argument('-i', '--input', type=argparse.FileType('rb'), metavar='FILE', help='file containing input distance matrix in condensed form')
@@ -284,30 +149,10 @@ if __name__ == '__main__':
     print("The method you selected with -m (%s) is not valid." % args.method)
     sys.exit(1)
 
-  if args.reorder_alg not in ["hungarian", "brute", "distance"]:
-    print("The reorder method you selected with --reorder-method (%s) is not valid." % args.reorder_alg)
-    sys.exit(1)
-
   if args.clusters_configurations:
     if args.clusters_configurations not in ["acr", "adf", "adfout", "alc", "arc", "bgf", "box", "bs", "c3d1", "c3d2", "cac", "caccrt", "cache", "cacint", "can", "car", "ccc", "cdx", "cdxml", "cht", "cif", "ck", "cml", "cmlr", "com", "copy", "crk2d", "crk3d", "csr", "cssr", "ct", "cub", "cube", "dmol", "dx", "ent", "fa", "fasta", "fch", "fchk", "fck", "feat", "fh", "fix", "fpt", "fract", "fs", "fsa", "g03", "g92", "g94", "g98", "gal", "gam", "gamin", "gamout", "gau", "gjc", "gjf", "gpr", "gr96", "gukin", "gukout", "gzmat", "hin", "inchi", "inp", "ins", "jin", "jout", "mcdl", "mcif", "mdl", "ml2", "mmcif", "mmd", "mmod", "mol", "mol2", "molden", "molreport", "moo", "mop", "mopcrt", "mopin", "mopout", "mpc", "mpd", "mpqc", "mpqcin", "msi", "msms", "nw", "nwo", "outmol", "pc", "pcm", "pdb", "png", "pov", "pqr", "pqs", "prep", "qcin", "qcout", "report", "res", "rsmi", "rxn", "sd", "sdf", "smi", "smiles", "sy2", "t41", "tdd", "test", "therm", "tmol", "txt", "txyz", "unixyz", "vmol", "xed", "xml", "xyz", "yob", "zin"]:
       print("The format you selected to save the clustered superposed configurations (%s) is not valid." % args.clusters_configurations)
       sys.exit(1)
-
-  if args.natoms_solute and not args.reorder:
-    print("Specifying the number of solute atoms is only useful for the reordering algorithms, continuing anyways..")
-    natoms = None
-  else:
-    natoms = args.natoms_solute
-
-  if args.reorder_alg == "hungarian":
-    reorder_alg = rmsd.reorder_hungarian
-  elif args.reorder_alg == "distance":
-    reorder_alg = rmsd.reorder_distance
-  elif args.reorder_alg == "brute":
-    reorder_alg = rmsd.reorder_brute
-
-  if not args.reorder:
-    reorder_alg = None
 
   if not args.input:
     if not args.outputdistmat:
@@ -331,7 +176,7 @@ if __name__ == '__main__':
   # build a distance matrix already in the condensed form
   else:
     print('\nCalculating distance matrix\n')
-    distmat = build_distance_matrix(args.trajectory_file, args.no_hydrogen, reorder_alg, natoms, args.nprocesses)
+    distmat = build_distance_matrix(args.trajectory_file, args.nprocesses)
     print('Saving condensed distance matrix to %s\n' % args.outputdistmat.name)
     np.savetxt(args.outputdistmat, distmat, fmt='%.18f')
 
@@ -347,7 +192,7 @@ if __name__ == '__main__':
   # get the elements closest to the centroid (see https://stackoverflow.com/a/39870085/3254658)
   if args.clusters_configurations:
     print("Writing superposed configurations per cluster to files %s\n" % (os.path.splitext(args.outputclusters.name)[0]+"_confs"+"_*"+"."+args.clusters_configurations))
-    save_clusters_config(args.trajectory_file, clusters, distmat, args.no_hydrogen, reorder_alg, natoms, os.path.splitext(args.outputclusters.name)[0]+"_confs", args.clusters_configurations)
+    save_clusters_config(args.trajectory_file, clusters, distmat, os.path.splitext(args.outputclusters.name)[0]+"_confs", args.clusters_configurations)
 
   if args.plot:
     # plot evolution with o cluster in trajectory
